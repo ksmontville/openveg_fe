d8346150ebda0a146ccd44633ddfee8b9c7ab456
Kyle <ksmontville@gmail.com>
2022-11-07 09:54:59 -0500

    Upgrade to Hobby dyno on Heroku.

--- src/components/toolbar/Filter.js
+++ src/components/toolbar/Filter.js
@@ -31,6 +31,11 @@ class Filter extends Component {
                     <input name="vegan" type="checkbox" value={this.state.vegan}
                            checked={this.state.vegan} onChange={this.handleChange}/>
                 </div>
+                
+                <p className={"text-lg font-bold m-4"}>
+                    NOTE: Currently only have data for New Hampshire, New York, Massachusetts, and Vermont.
+                </p>
+
             </div>
         )
     }
